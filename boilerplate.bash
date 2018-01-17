6bfaac67d2c0b92c62fe4b5982182147cdb79dcb
dryvenn <dryvenn@gmail.com>
2018-01-17 11:25:53 +0100

    Move .dotfiles/scripts/* .scripts/

---
+++ boilerplate.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+# set -o xtrace
+
+WD=$(pwd)
+SD=$(dirname $(readlink -e $0))
+
+err() {
+	1>&2 echo "$*"
+}
+
+usage() {
+	1>&2 cat <<- EOF
+	Usage: > $(basename $0) ...
+	EOF
+	exit 1
+}
